e273080757c38e2600cb773647f771fcac416621
Francisco Martínez Lasaca <frml@itu.dk>
2020-04-18 22:25:34 +0200

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="ros_launch_checker", # Replace with your own username
+    name="ros_launch_checker",  # Replace with your own username
     version="0.5.0",
     author="Francisco Martinez Lasaca",
     author_email="frml@itu.dk",
@@ -13,17 +13,17 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rosin-project/ros_launch_checker",
     packages=find_packages(exclude=['test']),
-    package_data= {
-            # Include txt files
-            "": ["*.txt"]
+    package_data={
+        # Include txt files
+        "": ["*.txt"]
     },
     install_requires=[
         "setuptools",
         "lxml",
         "colorama>=0.4.3",
     ],
-    entry_points = {
-        'console_scripts':[
+    entry_points={
+        'console_scripts': [
             'ros_launch_checker = ros_launch_checker.__main__:main'
         ]
     },
